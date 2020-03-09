b97ee2650130db88c045184f4f85077ceca63e35
Slava0135 <super.novalskiy_0135@inbox.ru>
2020-03-09 14:35:51 +0300

    PixelGame and MainMenuScreen init

---
+++ core/src/com/slava0135/pixelparty/MainMenuScreen.java
@@ -0,0 +1,40 @@
+package com.slava0135.pixelparty;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
+
+    final PixelGame game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final PixelGame game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Pixel Party!", 100, 150);
+        game.font.draw(game.batch, "Press anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+}
--- desktop/src/com/slava0135/pixelparty/desktop/DesktopLauncher.java
+++ desktop/src/com/slava0135/pixelparty/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.slava0135.pixelparty.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.slava0135.pixelparty.PixelGame;
@@ -7,6 +8,9 @@ import com.slava0135.pixelparty.PixelGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pixel Party";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new PixelGame(), config);
 	}
 }
