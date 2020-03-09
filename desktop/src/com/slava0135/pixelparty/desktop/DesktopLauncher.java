df4525712f8c5f87e251e61b77f377307097bcbd
Slava0135 <super.novalskiy_0135@inbox.ru>
2020-03-09 12:44:32 +0300

    Initial commit

---
+++ desktop/src/com/slava0135/pixelparty/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slava0135.pixelparty.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slava0135.pixelparty.PixelGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PixelGame(), config);
+	}
+}
