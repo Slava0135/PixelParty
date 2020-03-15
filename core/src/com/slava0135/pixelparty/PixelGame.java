80bd90c5b514bf783e931d56eb3e5baa2a130328
Slava0135 <super.novalskiy_0135@inbox.ru>
2020-03-15 16:23:39 +0300

    floor in MainMenu

--- core/src/com/slava0135/pixelparty/PixelGame.java
+++ core/src/com/slava0135/pixelparty/PixelGame.java
@@ -2,6 +2,7 @@ package com.slava0135.pixelparty;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
--- core/src/com/slava0135/pixelparty/Screens/MainMenuScreen.java
+++ core/src/com/slava0135/pixelparty/Screens/MainMenuScreen.java
@@ -5,31 +5,34 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.*;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.slava0135.pixelparty.PixelGame;
+import com.slava0135.pixelparty.World.Floor;
 
 public class MainMenuScreen implements Screen {
 
+    OrthographicCamera camera;
     private PixelGame game;
     private Stage stage;
     final static Color background = Color.WHITE;
+    private Floor floor = new Floor();
 
     public MainMenuScreen(final PixelGame game) {
         this.game = game;
         stage = new Stage(new ScreenViewport());
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1000, 1000);
 
         Label title = new Label("PIXEL PARTY", PixelGame.gameSkin);
         FreeTypeFontParameter parameter = new FreeTypeFontParameter();
-        parameter.size = 30;
+        parameter.size = 100;
         title.setStyle(new Label.LabelStyle(game.generator.generateFont(parameter), Color.BLACK));
         title.setAlignment(Align.center);
         title.setY(Gdx.graphics.getHeight()*0.9f);
@@ -51,20 +54,6 @@ public class MainMenuScreen implements Screen {
         });
         stage.addActor(playButton);
 
-        TextButton optionsButton = new TextButton("Options", PixelGame.gameSkin);
-        optionsButton.setWidth(Gdx.graphics.getWidth()/2f);
-        optionsButton.setPosition(Gdx.graphics.getWidth()/2f-optionsButton.getWidth()/2,Gdx.graphics.getHeight()/4f-optionsButton.getHeight()/2);
-        optionsButton.addListener(new InputListener(){
-            @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new OptionScreen(game));
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-        });
-        //stage.addActor(optionsButton);
     }
 
     @Override
@@ -76,6 +65,9 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(background.r, background.g, background.b, background.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        floor.generateFloor();
+        floor.draw(180, 180, 40);
+        camera.update();
         stage.act();
         stage.draw();
     }
