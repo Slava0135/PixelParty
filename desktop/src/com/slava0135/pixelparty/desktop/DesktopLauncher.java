742c4301c18743f5c7e84644145cb8948c0c78cb
Slava0135 <super.novalskiy_0135@inbox.ru>
2020-03-23 17:21:03 +0300

    updated to Lwjgl3

--- desktop/src/com/slava0135/pixelparty/desktop/DesktopLauncher.java
+++ desktop/src/com/slava0135/pixelparty/desktop/DesktopLauncher.java
@@ -1,16 +1,15 @@
 package com.slava0135.pixelparty.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.slava0135.pixelparty.PixelGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Pixel Party";
-		config.width = 1000;
-		config.height = 1000;
-		config.resizable = false;
-		new LwjglApplication(new PixelGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Pixel Party");
+		config.setWindowedMode(1000, 1000);
+		config.setWindowSizeLimits(1000, 1000, 1000, 1000);
+		new Lwjgl3Application(new PixelGame(), config);
 	}
 }
